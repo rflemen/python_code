9e851cbab580c87ac1d78b2ac3c0534428ea0581
rflemen <rflemen@gmail.com>
2023-02-24 11:37:46 -0500

    python_code

--- deck_of_cards_class.py
+++ deck_of_cards_class.py
@@ -1,4 +1,4 @@
-import random  # library for random number generation needed for shuffling deck
+import random  # library for random number generation needed for shuffling a deck of cards.
 
 # ----------CONSTANTS----------
 SUITS = ["clubs", "diamonds", "hearts", "spades"]
