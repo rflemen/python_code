05b8c8ead9d14189ea8feee78e0b3fa1b6f87a71
Rob Flemen <rflemen@gmail.com>
2024-07-30 21:04:21 -0400

    whatever

--- PCEP Book/4.3.1.6.py
+++ PCEP Book/4.3.1.6.py
@@ -1,16 +1,17 @@
 def is_year_leap(year):
-    if (year % 4 == 0 and year % 100 != 0):
-        return True
-    elif year % 400 == 0:
-        return True
-    else:
-        return False
+	if year % 4 == 0 and year % 100 != 0:
+		return True
+	elif year % 400 == 0:
+		return True
+	else:
+		return False
+
 
 test_data = [1900, 2000, 2016, 1987, 2020, 2024, 1997, 2015]
 test_results = [False, True, True, False, True, True, False, False]
 for i in range(len(test_data)):
 	yr = test_data[i]
-	print(yr,"->",end="")
+	print(yr, "->", end="")
 	result = is_year_leap(yr)
 	if result == test_results[i]:
 		print("OK")
