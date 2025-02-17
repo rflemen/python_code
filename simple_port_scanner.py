59941120bf018aa5b15d59e763eb5b6ba69679d6
Rob Flemen <rflemen@gmail.com>
2025-02-17 17:12:12 -0500

    Args added

--- simple_port_scanner.py
+++ simple_port_scanner.py
@@ -11,12 +11,17 @@ print("\t\t\t by Rob Flemen\n")
 #Take filename as argument
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="the file to be read")
+parser.add_argument("-p", "--port", help="the port to be scanned", type=int)
+parser.add_argument("-n", "--name", help="the name of the person running the program")
 args = parser.parse_args()
 print("The filename read in is:", args.filename)
-
+print("The port to be scanned is:", args.port)
+print("The name of the person running the program is:", args.name)
 
 # Argument (file name) fed to variable
 filename = args.filename
+username = args.name
+port = args.port
 
 
 # Read from a file (this will be for reading IP addresses to scan)
@@ -27,8 +32,9 @@ except FileNotFoundError:
     print(f"Sorry, the file {filename} does not exist.")
 
 
-# Determion if IP addresses in file are valid
-# REGEX pattern taken from "https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html"
+# Determion if IP addresses in file are valid IP addresses:
+# REGEX pattern taken from "https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html 
+# I did have to add an escape character "\" before the "\." for it to work completely correctly
 pattern = re.compile('''(^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)''')
 valid =[] 
 invalid=[] 
@@ -44,7 +50,7 @@ for ip in ips:
     else: 
       invalid.append(ip) 
   
-  
+
 # Displaying the IP addresses (just for debugging) 
 print("Valid IPs") 
 print(valid)
