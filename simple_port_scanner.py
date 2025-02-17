8af24c492e38ab6a44cf6477e09451425b49e62d
Rob Flemen <rflemen@gmail.com>
2025-02-17 17:04:53 -0500

    spacing changes

--- simple_port_scanner.py
+++ simple_port_scanner.py
@@ -7,6 +7,7 @@ banner = pyfiglet.figlet_format("Simple Port Scanner")
 print(banner)
 print("\t\t\t by Rob Flemen\n")
 
+
 #Take filename as argument
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="the file to be read")
@@ -17,6 +18,7 @@ print("The filename read in is:", args.filename)
 # Argument (file name) fed to variable
 filename = args.filename
 
+
 # Read from a file (this will be for reading IP addresses to scan)
 try:
     with open(filename) as file_object:
@@ -24,11 +26,13 @@ try:
 except FileNotFoundError:
     print(f"Sorry, the file {filename} does not exist.")
 
+
 # Determion if IP addresses in file are valid
 # REGEX pattern taken from "https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html"
 pattern = re.compile('''(^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)''')
 valid =[] 
 invalid=[] 
+
 
 for ip in ips: 
     ip = ip.rstrip() 
@@ -39,6 +43,7 @@ for ip in ips:
     # invalid IP addresses   
     else: 
       invalid.append(ip) 
+  
   
 # Displaying the IP addresses (just for debugging) 
 print("Valid IPs") 
